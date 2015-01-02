2e8fea81cdbefd886c76d8b0a43b305af53cb471
git <igarcia.troop@gmail.com>
2015-01-02 10:17:00 +0100

    Añadidos iconos rotosianos, mini-script con metadatos para actualizar y un poco de autobombo

---
+++ shurscript.meta.js
@@ -0,0 +1,33 @@
+// Shur Scripts SA
+// GPLv2 Licensed
+// http://www.gnu.org/licenses/gpl-2.0.html
+//
+// ==UserScript==
+// @name            ShurScript
+// @description     Script para ForoCoches
+// @icon            https://raw.github.com/igtroop/shurscript/anycloud/images/icon-48x48.png
+// @icon64          https://raw.github.com/igtroop/shurscript/anycloud/images/icon-64x64.png
+// @namespace       http://shurscript.es
+// @version         0.288.0
+// @author          TheBronx
+// @author          xusO
+// @author          Fritanga / Korrosivo
+// @author          Juno / ikaros45
+// @author          Electrosa
+// @author          igtroop
+// @include         http://www.forocoches.com*
+// @include         http://forocoches.com*
+// @grant           GM_log
+// @grant           GM_getValue
+// @grant           GM_setValue
+// @grant           GM_deleteValue
+// @grant           GM_xmlhttpRequest
+// @grant           GM_registerMenuCommand
+// @grant           GM_addStyle
+// @grant           GM_getResourceText
+// @grant           GM_getResourceURL
+// @grant           GM_getMetadata
+// @run-at          document-end
+// @updateURL       https://github.com/igtroop/shurscript/raw/anycloud/shurscript.user.js
+// @downloadURL     https://github.com/igtroop/shurscript/raw/anycloud/shurscript.user.js
+// ==/UserScript==
